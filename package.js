09f3212ed5beffc6d431fa15688e0615abc54ca2
martinbee <bee@Martins-MacBook-Pro.local>
2015-12-02 20:50:49 -0600

    modify package.js

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'poetic:react-velocityjs',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: "Meteor wrapper for Twitter-Fabric's velocity-react. Provides velocity component and helpers.",
+  git: 'https://github.com/poetic/react-velocityjs.git',
   documentation: 'README.md'
 });
 
@@ -16,9 +12,3 @@ Package.onUse(function(api) {
   api.addFiles('react-velocityjs.js');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('poetic:react-velocityjs');
-  api.addFiles('react-velocityjs-tests.js');
-});
